071e4eee8d39736b85106d11cfde508e0f97ae32
Richard Turnbull <richard.turnbull@gmail.com>
2010-03-10 12:51:19 +0000

    added scripts directory, and the tag.sh script

---
+++ scripts/tag.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+product="bkp-rsync"
+
+version=$1
+if [ "$version" == "" ]; then
+	echo "usage: $0 <version>"
+	exit
+fi
+
+git checkout master
+
+status=`git status --porcelain`
+if [ "$status" != "" ];  then
+	echo "** Tree not ready. stopping"
+#	exit
+fi
+
+# Tag
+tag="$product-$version"
+git tag -m "Release tag for version $version" -a $tag
+git push origin master
+git push --tags
+
+
+
