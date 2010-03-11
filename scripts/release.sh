8fe4c8b47fae4f83bc43d4b0c8d4edd97d89f215
Richard Turnbull <richard.turnbull@gmail.com>
2010-03-11 13:42:16 +0000

    added a script to do a release

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,37 +6,44 @@
 # updates __VERSION__ and __DATE__
 # zips up the stuff
 
-product="bkp-rsync"
+
 # Configuration
+product="bkp-rsync"
 release_folder=~/Development/Releases/$product
 build_folder=~/Development/Build/$product
 
-if [ $version == "" ]; then 
+echo "rel_folder=$release_folder"
+
+version=$1
+
+if [ "$version" == "" ]; then 
 	echo "usage: $0 <version>"
 	exit
 fi
 
 # must be run from project root
-if [ ! -d "./.git"]; then
+if [ ! -d "./.git" ]; then 
 	echo "must be run from project root folder"
 	exit
 fi
 
 #create directories if they don't exist
-if [ ! -d $release_folder]; then
-	mkdir -p $release_folder
+if [ ! -d "$release_folder" ]; then
+	mkdir -p "$release_folder"
 fi
 
-if [ ! -d $build_folder]; then
-	mkdir -p $build_folder
+if [ ! -d $build_folder ]; then
+	mkdir -p "$build_folder"
 fi
 
 # create the destination folder which will contain the actual build
-$destination_folder="$build_folder/$product-$version"
+destination_folder="$build_folder/$product-$version"
+echo "destination_folder = $destination_folder"
 if [ -d $desination_folder ]; then
 	echo "deleting existing $destination_folder"
-	rm -r "$build_folder/$product-$version"
+	rm -r "$destination_folder"
 fi
+
 mkdir $destination_folder
 
 #checkout the tag we want to build
@@ -47,18 +54,27 @@ git checkout $tag
 cp -R * $destination_folder
 
 #delete the build_scripts folder
-rm -r "$build_folder/$product-$version/scripts"
+echo "removing build scripts"
+rm -r "$destination_folder/scripts"
 
 # Replace the version number and date
-release_date = `date "+%a, %d %h %Y %T %z"`
+release_date=`date "+%a %d %h %Y %T"`
+echo "release date = $release_date"
+echo "version = $version"
+cd $destination_folder
 
-sed -i "" 's/__VERSION__/'$version'/g' $destination_folder/bkp_rsync.pl
-sed -i "" 's/__DATE__/'$release_date'/g' $destination_folder/bkp_rsync.pl
+version_string="Version: $version"
+date_str
+
+sed -i "" 's/__VERSION__/'"$version_string"'/g' bkp-rsync.pl
+sed -i "" 's/__DATE__/'"$release_date"'/g' bkp-rsync.pl
 
 # zip up
 cd $build_folder
+ls
 tar cvfz "$product-$version.tgz" "$product-$version"
-
+mv "$product-$version.tgz" $release_folder
+rm -r $destination_folder
 
 
 
